4da4f9ecbc4ddb5088fab8f06385ab4cb097409f
kyle <kcollie@hotmail.com>
2015-05-25 23:40:55 -0400

    no changes

---
+++ LoopXML/Book.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LoopXML
+{
+    class Book
+    {
+        public string id { get; set; }
+        public string author {get;set;}
+        public string title {get;set;}
+        public string genre {get;set;}
+        public decimal price {get;set;}
+        public DateTime publishDate {get;set;}
+        public string description {get;set;}
+
+        public Book()
+        {
+            this.id = null;
+            this.author = null;
+            this.title = null;
+            this.genre = null;
+            this.price = 0;
+            this.publishDate = DateTime.MinValue;
+            this.description = null;
+        }
+
+        public Book(string id, string a, string t, string g, decimal p, DateTime pd, string d)
+        {
+            this.id = id;
+            this.author = a;
+            this.title = t;
+            this.genre = g;
+            this.price = p;
+            this.publishDate = pd;
+            this.description = d;
+        }
+    }
+}
---
+++ LoopXML/BookList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LoopXML
+{
+    public partial class frmBookList : Form
+    {
+        List<Book> BookList = new List<Book>();
+        Queue<Book> bookQueue = new Queue<Book>();
+        public frmBookList()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bookQueue.Count > 0)
+            {
+                var book = bookQueue.Dequeue();
+                textBox1.Text = book.title;
+                textBox2.Text = book.price.ToString();
+                label3.Text = "Books in list: " + bookQueue.Count.ToString();
+
+            }
+            else
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                button1.Enabled = false;
+                //MessageBox.Show("That's all folks!");
+            }
+        }
+
+        private void frmBookList_Load(object sender, EventArgs e)
+        {
+            
+            XmlDocument XmlDoc = new XmlDocument();
+            XmlDoc.Load(@"C:\Users\Kyle\Documents\Visual Studio 2013\Projects\books.xml");
+            XmlNodeList XmlDocNodes = XmlDoc.DocumentElement.SelectNodes("/catalog/book");
+            
+            foreach (XmlNode node in XmlDocNodes)
+            {
+                Book obj = new Book(
+                    node.Attributes["id"].Value,
+                    node["author"].InnerText,
+                    node["title"].InnerText,
+                    node["genre"].InnerText,
+                    Convert.ToDecimal(node["price"].InnerText),
+                    Convert.ToDateTime(node["publish_date"].InnerText),
+                    node["description"].InnerText
+                    );
+                BookList.Add(obj);
+            }
+            loadQue();
+        }
+
+        private void loadQue()
+        {
+            bookQueue.Clear();
+            foreach (Book b in BookList)
+            {
+                bookQueue.Enqueue(b);
+            }
+
+            label3.Text = "Books in list: " + BookList.Count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadQue();
+            textBox1.Clear();
+            textBox2.Clear();
+            button1.Enabled = true;
+        }
+    }
+}
